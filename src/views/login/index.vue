fa0a84ab100d68d41bff98b997779879e443fd7f
lidongli <1344066958@qq.com>
2019-06-01 13:35:44 +0800

    修改登录

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -24,6 +24,10 @@
         />
       </el-form-item>
 
+      <el-form-item>
+        <el-button>我是新按钮</el-button>
+      </el-form-item>
+
       <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
         <el-form-item prop="password">
           <span class="svg-container">
